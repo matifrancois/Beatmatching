93adbbc7288976711e76d42af8c87818dc1a89a7
Matias Francois <mfrancois@itba.edu.ar>
2022-05-19 13:37:36 -0300

    api documented

---
+++ frontend/src/App.js
@@ -0,0 +1,217 @@
+import './App.css';
+import * as React from 'react';
+import Slider from '@mui/material/Slider';
+import Box from '@mui/material/Box';
+import NumericInput from 'react-numeric-input';
+import 'react-h5-audio-player/lib/styles.css';
+
+
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+
+var SliderTiempo_value = 16;
+var SliderAtenuacion_value = 64;
+var transition_time = 10;
+var song1 = ""
+var song2 = ""
+
+function changeSong1(value){
+  console.log(value)
+  song1 = value;
+}
+
+function changeSong2(value){
+  song2 = value;
+}
+
+function SliderTiempo(value) {
+  SliderTiempo_value = value;
+}
+
+function SliderAtenuacion(value) {
+  SliderAtenuacion_value = value;
+}
+
+function NumericInputChange(value) {
+  transition_time = value;
+}
+
+
+
+// var songs = [
+//   {
+//     "id": 1,
+//     "path": "daniela_andrade",
+//     "title": "Crazy Daniela Andrade",
+//     "image_path": "https://i.scdn.co/image/ab67616d00001e0229455064ffc25216a8a576b2",
+//   },
+//   {
+//     "id": 2,
+//     "path": "Gnarls-Barkley-Crazy_cortado",
+//     "title": "Crazy Gnarls Barkley",
+//     "image_path": "https://m.media-amazon.com/images/I/81mWl+Yr5nL._SS500_.jpg",
+//   },
+//   {
+//     "id": 2,
+//     "path": "starwars",
+//     "title": "Star Wars",
+//     "image_path": "https://images.unsplash.com/photo-1618336753974-aae8e04506aa?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+//   },
+//   {
+//     "id": 4,
+//     "path": "goingup60_mono",
+//     "title": "Going Up",
+//     "image_path": "https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+//   }
+
+// ]
+
+
+// function Send_data(){
+//   console.log("sending data: {song1: " + song1 + ", song2: " + song2 + ", SliderTiempo: " + SliderTiempo_value.toString() + ", SliderAtenuacion: " + SliderAtenuacion_value.toString() + ", NumericInputChange: " +  transition_time.toString() + "}");
+//   fetch(
+//     "http://localhost:8000/mix?song_1=" + song1 + "&song_2=" + song2 + "&f_second=" + SliderTiempo_value.toString() + "&time_to_switch=" + transition_time.toString() + "&attenuation=" + SliderAtenuacion_value.toString())
+//                 .then((res) => res.json())
+//                 .then((json) => {
+//                     console.log(json);
+//                     // my_Audio();
+//                     });
+// }
+
+
+
+
+// function my_Audio() {
+//   document.getElementById("mp4_src").src =
+// "results2.wav"; 
+
+//   document.getElementById("my_Audio").load();
+// }
+
+function App() {
+  const [path, setpath] = React.useState("results2.wav");
+  const [songs, setsongs] = React.useState([]);
+
+  
+
+  React.useEffect(() => {
+    setpath("results.wav");
+    fetch(
+      "http://localhost:8000/songs")
+                  .then((res) => res.json())
+                  .then((json) => {
+                      setsongs(json);
+                      });
+  }, []);
+
+  var rows = [];
+for (const song in songs) {
+    rows.push(
+      <a className="card" onClick={() => changeSong1(songs[song].path)}>
+            <div className="card_background" style={{ backgroundImage: "url(" + songs[song].image_path + ")"}}>
+</div>
+            <div className="card_content">
+              <p className="card_category">{songs[song].bpm} BPMs</p>
+              <h3 className="card_heading">{songs[song].title}</h3>
+            </div>
+          </a>
+    );
+}
+
+var rows2 = [];
+for (const song in songs) {
+  rows2.push(
+      <a className="card" onClick={() => changeSong2(songs[song].path)}>
+            <div className="card_background" style={{ backgroundImage: "url(" + songs[song].image_path + ")"}}>
+</div>
+            <div className="card_content">
+              <p className="card_category">{songs[song].bpm} BPMs</p>
+              <h3 className="card_heading">{songs[song].title}</h3>
+            </div>
+          </a>
+    );
+}
+
+  const Send_datados = () => {
+    console.log("sending data: {song1: " + song1 + ", song2: " + song2 + ", SliderTiempo: " + SliderTiempo_value.toString() + ", SliderAtenuacion: " + SliderAtenuacion_value.toString() + ", NumericInputChange: " +  transition_time.toString() + "}");
+    fetch(
+      "http://localhost:8000/mix?song_1=" + song1 + "&song_2=" + song2 + "&f_second=" + transition_time.toString() + "&time_to_switch=" + SliderTiempo_value.toString() + "&attenuation=" + SliderAtenuacion_value.toString())
+                  .then((res) => res.json())
+                  .then((json) => {
+                      console.log(json);
+                      setpath("results.wav"); 
+                      });
+  }
+
+  return (
+    <div>
+
+    <section className="hero-section">
+      
+      <div className="grid">
+
+        {/****************** Listas de canciones *******************/}
+        <div  style={{display:"flex", justifyContent: "center", alignItems: "center", color: "white", fontSize: "30px"}}>
+          <h1>Sound Mixer</h1>
+        </div>
+        <div  style={{display:"flex", justifyContent: "center", alignItems: "center", color: "gray", fontSize: "30px"}}>
+          <span>Seleccione 2 canciones a mezclar y los parámetros necesarios para la reproducción.</span>
+        </div>
+        <h2>Primera canción</h2>
+
+        <tbody className="card-grid">{rows}</tbody>
+          
+        <h2>Segunda canción</h2>
+
+        <tbody className="card-grid">{rows2}</tbody>
+
+        {/****************** Sector configuración *******************/}
+        <div className="card-grid">
+        </div>
+        <div style={{display:"flex", justifyContent: "center", alignItems: "center"}}>
+          <Box width={500}>
+            <div style={{display:"flex", justifyContent: "center", alignItems: "center", color: "white", fontSize: "20px"}}>
+              <h2 style={{marginBottom: "40px", marginTop: "40px"}}>Selección de parámetros</h2>
+            </div>
+            <h2>Tiempo para el cambio de canción (segundos)</h2>
+            <Slider defaultValue={16} aria-label="Default" valueLabelDisplay="auto" getAriaValueText={SliderTiempo} marks min={1}
+          max={32}/>
+            <h2>Atenuación en el cambio (veces)</h2>
+            <Slider defaultValue={64} aria-label="Default" valueLabelDisplay="auto" step={1} marks min={4}
+          max={128} getAriaValueText={SliderAtenuacion}/>
+            <h2 style={{marginBottom: "20px"}}>Segundo donde comienza a ocurrir el cambio</h2>
+            <div style={{display:"flex", justifyContent: "center", alignItems: "center"}}>
+            <NumericInput min={0} max={100} value={10} style={{
+            input: {
+              height: "30px"
+              }
+            }} onChange={NumericInputChange}/>
+            </div>
+            <br></br>
+            <div style={{justifyContent:"center", display: "flex"}}>
+              <Button style={{marginTop: "20px", justifyContent: 'center', width: "30%", height: "40px"}} variant="contained" endIcon={<SendIcon />} onClick={Send_datados}>
+                Enviar
+              </Button>
+            </div>
+            <div style={{justifyContent:"center", display: "flex", color: "white", fontSize: "20px"}}>
+              <h1 style={{marginTop: "30px"}}>Audio resultante</h1>
+            </div>
+            <div style={{justifyContent:"center", display: "flex"}}>
+              <audio
+              id="my_Audio"
+              style={{width: "100%", marginTop: "20px"}}
+              controls
+              src={path}>
+                <source id="mp4_src" src={"results2.wav"} type="audio/mp3">
+                </source>
+              </audio>
+            </div>
+          </Box>
+        </div>
+      </div>
+    </section>
+  </div>
+  );
+}
+
+export default App;
--- python_api/main.py
+++ python_api/main.py
@@ -1,24 +1,20 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import numpy as np
-import pytsmod as tsm
 import soundfile as sf  # you can use other audio load packages.
-import os
-
+from helper import wsola
 from fastapi.middleware.cors import CORSMiddleware
-
-# import numpy as np
-from scipy.interpolate import interp1d
-# from .utils import win as win_func
-# from .utils import _validate_audio, _validate_scale_factor
-
+import numpy as np
 from math import log
 
+# We define the FastApi module
+
+app = FastAPI()
+
+###### This part is needed in order to connect with the frontend #####
+ 
 origins = [
     "http://localhost:3000",
 ]
 
-app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,218 +24,42 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+###### this is like the songs database #####
+
+# This part must migrate to a real database in furter improvements
+
 songs_names = [
     {
-        "id": 1,
-        "name": "star wars",
-        "path": "starwars.wav"
-    },
-    {
-        "id": 2,
-        "name": "Crazy daniela andrade",
-        "path": "daniela_andrade.wav"
-    },
-    {
-        "id": 3,
-        "name": "Crazy Gnarls Barkley",
-        "path": "Gnarls-Barkley-Crazy_cortado.wav"
-    },
-    ]
+    "id": 1,
+    "path": "daniela_andrade",
+    "title": "Crazy Daniela Andrade",
+    "image_path": "https://i.scdn.co/image/ab67616d00001e0229455064ffc25216a8a576b2",
+    "bpm": 99,
+  },
+  {
+    "id": 2,
+    "path": "Gnarls-Barkley-Crazy_cortado",
+    "title": "Crazy Gnarls Barkley",
+    "image_path": "https://m.media-amazon.com/images/I/81mWl+Yr5nL._SS500_.jpg",
+    "bpm": 112,
+  },
+  {
+    "id": 2,
+    "path": "starwars",
+    "title": "Star Wars",
+    "image_path": "https://images.unsplash.com/photo-1618336753974-aae8e04506aa?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170",
+    "bpm": 99,
+  },
+  {
+    "id": 4,
+    "path": "goingup60_mono",
+    "title": "Going Up",
+    "image_path": "https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    "bpm": 116,
+  }
+]
 
-
-def wsola(x, s, win_type='hann',
-          win_size=1024, syn_hop_size=512, tolerance=512):
-    """Modify length of the audio sequence using WSOLA algorithm.
-    Parameters
-    ----------
-    x : numpy.ndarray [shape=(channel, num_samples) or (num_samples)]
-        the input audio sequence to modify.
-    s : number > 0 [scalar] or numpy.ndarray [shape=(2, num_points)]
-        the time stretching factor. Either a constant value (alpha)
-        or an 2 x n array of anchor points which contains the sample points
-        of the input signal in the first row
-        and the sample points of the output signal in the second row.
-    win_type : str
-               type of the window function. hann and sin are available.
-    win_size : int > 0 [scalar]
-               size of the window function.
-    syn_hop_size : int > 0 [scalar]
-                   hop size of the synthesis window.
-                   Usually half of the window size.
-    tolerance : int >= 0 [scalar]
-                number of samples the window positions
-                in the input signal may be shifted
-                to avoid phase discontinuities when overlap-adding them
-                to form the output signal (given in samples).
-    Returns
-    -------
-    y : numpy.ndarray [shape=(channel, num_samples) or (num_samples)]
-        the modified output audio sequence.
-    """
-    # validate the input audio and scale factor.
-    x = _validate_audio(x)
-    anc_points = _validate_scale_factor(x, s)
-
-    n_chan = x.shape[0]
-    output_length = int(anc_points[-1, -1]) + 1
-
-    win = wind(win_type=win_type, win_size=win_size, zero_pad=0)
-
-    sw_pos = np.arange(0, output_length + win_size // 2, syn_hop_size)
-    ana_interpolated = interp1d(anc_points[1, :], anc_points[0, :],
-                                fill_value='extrapolate')
-    aw_pos = np.round(ana_interpolated(sw_pos)).astype(int)
-    ana_hop = np.insert(aw_pos[1:] - aw_pos[0: -1], 0, 0)
-
-    y = np.zeros((n_chan, output_length))
-
-    min_fac = np.min(syn_hop_size / ana_hop[1:])
-
-    # padding the input audio sequence.
-    left_pad = int(win_size // 2 + tolerance)
-    right_pad = int(np.ceil(1 / min_fac) * win_size + tolerance)
-    x_padded = np.pad(x, ((0, 0), (left_pad, right_pad)), 'constant')
-
-    aw_pos = aw_pos + tolerance
-
-    # Applying WSOLA to each channels
-    for c, x_chan in enumerate(x_padded):
-        y_chan = np.zeros(output_length + 2 * win_size)
-        ow = np.zeros(output_length + 2 * win_size)
-
-        delta = 0
-
-        for i in range(len(aw_pos) - 1):
-            x_adj = x_chan[aw_pos[i] + delta: aw_pos[i] + win_size + delta]
-            y_chan[sw_pos[i]: sw_pos[i] + win_size] += x_adj * win
-            ow[sw_pos[i]: sw_pos[i] + win_size] += win
-
-            nat_prog = x_chan[aw_pos[i] + delta + syn_hop_size:
-                              aw_pos[i] + delta + syn_hop_size + win_size]
-
-            next_aw_range = np.arange(aw_pos[i+1] - tolerance,
-                                      aw_pos[i+1] + win_size + tolerance)
-
-            x_next = x_chan[next_aw_range]
-
-            cross_corr = np.correlate(nat_prog, x_next)
-            max_index = np.argmax(cross_corr)
-
-            delta = tolerance - max_index
-
-        # Calculate last frame
-        x_adj = x_chan[aw_pos[-1] + delta: aw_pos[-1] + win_size + delta]
-        y_chan[sw_pos[-1]: sw_pos[-1] + win_size] += x_adj * win
-        ow[sw_pos[-1]: sw_pos[-1] + win_size] += + win
-
-        ow[ow < 1e-3] = 1
-
-        y_chan = y_chan / ow
-        y_chan = y_chan[win_size // 2:]
-        y_chan = y_chan[: output_length]
-
-        y[c, :] = y_chan
-
-    return y.squeeze()
-
-
-def wind(win_type='hann', win_size=4096, zero_pad=0):
-    """Generate diverse type of window function
-    Parameters
-    ----------
-    win_type : str
-               the type of window function.
-               Currently, Hann and Sin are supported.
-    win_size : int > 0 [scalar]
-               the size of window function.
-               It doesn't contains the length of zero padding.
-    zero_pad : int > 0 [scalar]
-               the total length of zero-pad.
-               Zeros are equally distributed
-               for both left and right of the window.
-    Returns
-    -------
-    win : numpy.ndarray([shape=(win_size)])
-          the window function generated.
-    """
-
-    if win_type == 'hann':
-        win = np.hanning(win_size)
-    elif win_type == 'sin':
-        win = np.sin(np.pi * np.arange(win_size) / (win_size - 1))
-    else:
-        raise Exception("Please use the valid window type. (hann, sin)")
-
-    win = np.pad(win, zero_pad // 2, 'constant')
-
-    return win
-
-
-def _validate_audio(audio):
-    """validate the input audio and modify the order of channels.
-    Parameters
-    ----------
-    audio : numpy.ndarray [shape=(channel, num_samples) or (num_samples)\
-                           or (num_samples, channel)]
-            the input audio sequence to validate.
-    Returns
-    -------
-    audio : numpy.ndarray [shape=(channel, num_samples)]
-            the validataed output audio sequence.
-    """
-    if audio.ndim == 1:
-        audio = np.expand_dims(audio, 0)
-    elif audio.ndim > 2:
-        raise Exception("Please use the valid audio source. "
-                        + "Number of dimension of input should be less than 3.")
-    elif audio.shape[0] > audio.shape[1]:
-        warn('it seems that the 2nd axis of the input audio source '
-             + 'is a channel. it is recommended that fix channel '
-             + 'to the 1st axis.', stacklevel=3)
-        audio = audio.T
-
-    return audio
-
-
-def _validate_scale_factor(audio, s):
-    """Validate the scale factor s and
-    convert the fixed scale factor to anchor points.
-    Parameters
-    ----------
-    audio : numpy.ndarray [shape=(num_channels, num_samples) \
-                           or (num_samples) or (num_samples, num_channels)]
-            the input audio sequence.
-    s : number > 0 [scalar] or numpy.ndarray [shape=(2, num_points) \
-        or (num_points, 2)]
-        the time stretching factor. Either a constant value (alpha)
-        or an (2 x n) (or (n x 2)) array of anchor points
-        which contains the sample points of the input signal in the first row
-        and the sample points of the output signal in the second row.
-    Returns
-    -------
-    anc_points : numpy.ndarray [shape=(2, num_points)]
-                 anchor points which contains the sample points
-                 of the input signal in the first row
-                 and the sample points of the output signal in the second row.
-    """
-    if np.isscalar(s):
-        anc_points = np.array([[0, np.shape(audio)[1] - 1],
-                               [0, np.ceil(s * np.shape(audio)[1]) - 1]])
-    elif s.ndim == 2:
-        if s.shape[0] == 2:
-            anc_points = s
-        elif s.shape[1] == 2:
-            warn('it seems that the anchor points '
-                 + 'has shape (num_points, 2). '
-                 + 'it is recommended to '
-                 + 'have shape (2, num_points).', stacklevel=3)
-            anc_points = s.T
-    else:
-        raise Exception('Please use the valid anchor points. '
-                        + '(scalar or pair of input/output sample points)')
-
-    return anc_points
-
+######## Endpoints ########
 
 @app.get("/")
 def root():
@@ -251,51 +71,72 @@ def get_songs():
 
 @app.get("/mix")
 def get_mix(song_1: str, song_2: str, f_second: int, time_to_switch: int, attenuation: int):
-    x, sr = sf.read(song_1 + '.wav') # daniela_andrade.wav
-    a, sr_a = sf.read(song_2 + '.wav') # Gnarls-Barkley-Crazy_cortado.wav
 
+    # This part is to load the audio files
 
-    print(f_second)
-    print(time_to_switch)
+    x, sr = sf.read(song_1 + '.wav')
+    a, sr_a = sf.read(song_2 + '.wav')
+
+    bpm_1 = 0
+    bpm_2 = 0
+
+    for song in songs_names:
+        if(song["path"] == song_1):
+            bpm_1 = song["bpm"]
+        elif(song["path"] == song_2):
+            bpm_2 = song["bpm"]
+        
+    if bpm_1 == 0 or bpm_2 == 0:
+        return {"error": "At least one of the songs is not in the database"}
+    # print(f_second)
+    # print(time_to_switch)
 
     x = x.T
     x_length = x.shape[-1]  # length of the audio sequence x.
 
-    first_second = f_second #  25 Segundo donde empieza a cambiar
-    # time_to_switch = 6 # tiempo que tarda en cambiar
-    samples_to_switch = time_to_switch*sr
-    # number_of_changes = 7 # numero de cambios en velocidad que performea
-    target_s = 0.68 # valor de velocidad buscado
-    step = 1*sr # numero de samples luego de los cuales se realiza un cambio de velocidad
-    veces_de_reducción = attenuation # 64.0
-    logaritm = log(veces_de_reducción) / log(2)
-
+    first_second = f_second # First second of the switching part
+    # time_to_switch = time in the song switch
+    samples_to_switch = time_to_switch*sr # number of samples in the switch
+    target_s = bpm_1/bpm_2 # BPMs relationship (BPM1/BPM2)
+    step = 1*sr # Number of samples to change the speed
+    veces_de_reduccion = attenuation # attenuation in the switch (is the number of times the song is reduced at the end throw a logaritmic function)
+    
+    # This part is to calculate some helper variables to the logaritmic function
+    logaritm = log(veces_de_reduccion) / log(2)
     pendiente_del_log = logaritm/samples_to_switch
 
-    print(pendiente_del_log)
+    # print(pendiente_del_log)
 
+    # This part is to calculate the number of changes of speed in the switching part
     number_of_changes = int(samples_to_switch / step)
-    print(number_of_changes)
+    # print(number_of_changes)
 
-    y = [] # vector con vectores correspondientes a las separaciones
+    ###### Here we separate the son in parts accordingly with the number of changes in the switch ######
+
+    y = [] # array of arrays with the parts of the song 
 
     y.append(x[0:first_second*sr])
     for i in range(number_of_changes):
         y.append(x[first_second*sr+i*step:first_second*sr+(i+1)*step])
 
-    y.append(x[first_second*sr+number_of_changes*step:x_length])
+    #y.append(x[first_second*sr+number_of_changes*step:x_length])
 
-    print(len(y))
+    # print(len(y))
 
+    ###### Here we calculate the factor of speed for every part of the song in the switching part ######
+
+    # first we calculate the speed_step
     speed_step = (1-target_s) / number_of_changes
-    print(speed_step)
+    # print(speed_step)
 
-    speeds=[]
+    speeds=[] # array of the speeds for every part of the song in the switching part
 
     for i in range(number_of_changes+1):
         speeds.append(1-i*speed_step)
 
     speeds.append(1)
+
+    ##### Here we calculate the output of the wsola function for every song part in the switch with the correspondent speed factor #####
 
     z = []
 
@@ -304,8 +145,10 @@ def get_mix(song_1: str, song_2: str, f_second: int, time_to_switch: int, attenu
     for i in range(number_of_changes):
         z.append(wsola(y[i+1], speeds[i+1]))
 
-    z.append(y[number_of_changes+1])
-    print(len(z))
+    #z.append(y[number_of_changes+1])
+    # print(len(z))
+
+    ##### Here we concatenate every part of the modified song ######
 
     output = []
 
@@ -314,18 +157,21 @@ def get_mix(song_1: str, song_2: str, f_second: int, time_to_switch: int, attenu
     for i in range(number_of_changes):
         output = np.concatenate((output, z[i+1]))
 
-    output = np.concatenate((output, z[number_of_changes+1]))
+    #output = np.concatenate((output, z[number_of_changes+1]))
 
-    for i in range(samples_to_switch):
+    ##### Here we calculate the attenuation for every part of the song in the switch ######
+
+    for i in range(len(output)- len(z[0])):
         output[i+first_second*sr] = output[i+first_second*sr] / (2**(i*pendiente_del_log)) + a[i] / (2**(
                 (samples_to_switch-i)*pendiente_del_log))
     # print(len(output))
     # print(len(a))
     # print(len(x))
-    for i in range(900000):
-        output[first_second*sr + samples_to_switch + i + 1] = a[samples_to_switch + i]
+    # for i in range(900000):
+    #     output[first_second*sr + samples_to_switch + i + 1] = a[samples_to_switch + i]
+    output = np.concatenate((output, a[len(output)- len(z[0]):]))
 
-
+    ##### Here we write the correspondent file and return a message to indicate that every works fine ######
 
     sf.write("../frontend/public/results.wav", output, sr)
 
